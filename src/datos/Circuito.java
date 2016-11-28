85e3dfd6ff33986efafc467967d5baa8c1148772
DAVID <davidminguez97@opendeusto.es>
2016-11-28 20:24:30 +0100

    .

--- src/datos/Circuito.java
+++ src/datos/Circuito.java
@@ -8,101 +8,101 @@ import java.time.Duration;
 
 public class Circuito {
 
-    protected String nom_circuito;
-    protected String pais;
-    protected int vueltas;
-    protected double probLluvia;
-    protected String fotoCircuito;
-    protected  long rangoTiempoInicial;
-    protected long rangoTiempoFinal;
+	 protected String nom_circuito;
+	    protected String pais;
+	    protected int num_vueltas;
+	    protected double pro_lluvia;
+	    protected String foto_circ;
+	    protected  int rango_tinicial;
+	    protected int rango_tfinal;
 
 
-    /**
-     * Contructor vacio con valores predeterminados
-     */
-    public Circuito(){
-        nombre = " ";
-        pais = " ";
-        probLluvia = 0;
-    }
+	    /**
+	     * Contructor vacio con valores predeterminados
+	     */
+	    public Circuito(){
+	        nom_circuito = " ";
+	        pais = " ";
+	        pro_lluvia = 0;
+	    }
 
-    /**
-     * Contructor para la creacion personalizada de un circuito
-     * @param nombre
-     * @param pais
-     * @param vueltas
-     * @param probLluvia
-     * @param fotoCircuito
-     * @param rangoTiempoInicial
-     * @param rangoTiempoFinal
-     */
-    public Circuito(String nombre, String pais, int vueltas, double probLluvia, String fotoCircuito, long rangoTiempoInicial, long rangoTiempoFinal) {
-        this.nombre = nombre;
-        this.pais = pais;
-        this.vueltas = vueltas;
-        this.probLluvia = probLluvia;
-        this.fotoCircuito = fotoCircuito;
-        this.rangoTiempoInicial = rangoTiempoInicial;
-        this.rangoTiempoFinal = rangoTiempoFinal;
-    }
+	    /**
+	     * Contructor para la creacion personalizada de un circuito
+	     * @param nombre
+	     * @param pais
+	     * @param vueltas
+	     * @param probLluvia
+	     * @param fotoCircuito
+	     * @param rangoTiempoInicial
+	     * @param rangoTiempoFinal
+	     */
+	    public Circuito(String nombre, String pais, int vueltas, double probLluvia, String fotoCircuito, int rangoTiempoInicial, int rangoTiempoFinal) {
+	        this.nom_circuito = nombre;
+	        this.pais = pais;
+	        this.num_vueltas = vueltas;
+	        this.pro_lluvia = probLluvia;
+	        this.foto_circ = fotoCircuito;
+	        this.rango_tinicial = rangoTiempoInicial;
+	        this.rango_tfinal = rangoTiempoFinal;
+	    }
 
-    public String getNombre() {
-        return nombre;
-    }
+	    public String getNombre() {
+	        return nom_circuito;
+	    }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	    public void setNombre(String nombre) {
+	        this.nom_circuito = nombre;
+	    }
 
-    public String getPais() {
-        return pais;
-    }
+	    public String getPais() {
+	        return pais;
+	    }
 
-    public void setPais(String pais) {
-        this.pais = pais;
-    }
+	    public void setPais(String pais) {
+	        this.pais = pais;
+	    }
 
-    public int getVueltas() {
-        return vueltas;
-    }
+	    public int getVueltas() {
+	        return num_vueltas;
+	    }
 
-    public void setVueltas(int vueltas) {
-        this.vueltas = vueltas;
-    }
+	    public void setVueltas(int vueltas) {
+	        this.num_vueltas = vueltas;
+	    }
 
-    public double getProbLluvia() {
-        return probLluvia;
-    }
+	    public double getProbLluvia() {
+	        return pro_lluvia;
+	    }
 
-    public void setProbLluvia(double probLluvia) {
-        this.probLluvia = probLluvia;
-    }
+	    public void setProbLluvia(double probLluvia) {
+	        this.pro_lluvia = probLluvia;
+	    }
 
-    public String getFotoCircuito() {
-        return fotoCircuito;
-    }
+	    public String getFotoCircuito() {
+	        return foto_circ;
+	    }
 
-    public void setFotoCircuito(String fotoCircuito) { this.fotoCircuito = fotoCircuito;
-    }
+	    public void setFotoCircuito(String fotoCircuito) { this.foto_circ = fotoCircuito;
+	    }
 
-    public long getRangoTiempoInicial() {  return rangoTiempoInicial;
-    }
+	    public long getRangoTiempoInicial() {  return rango_tinicial;
+	    }
 
-    public void setRangoTiempoInicial(long rangoTiempoInicial) {  this.rangoTiempoInicial = rangoTiempoInicial;
-    }
+	    public void setRangoTiempoInicial(int rangoTiempoInicial) {  this.rango_tinicial = rangoTiempoInicial;
+	    }
 
-    public long getRangoTiempoFinal() {    return rangoTiempoFinal;
-    }
+	    public long getRangoTiempoFinal() {    return rango_tfinal;
+	    }
 
-    public void setRangoTiempoFinal(long rangoTiempoFinal) {  this.rangoTiempoFinal = rangoTiempoFinal;
-    }
+	    public void setRangoTiempoFinal(int rangoTiempoFinal) {  this.rango_tfinal = rangoTiempoFinal;
+	    }
 
-    public static void main(String[] args){
-        Circuito c = new Circuito();
-        /*c.rangoTiempo[0] = c.rangoTiempo[0].ofSeconds(3600);
-        System.out.println(c.rangoTiempo[0].toHours());
-        System.out.println(c.rangoTiempo[0].toMinutes());*/
+	    public static void main(String[] args){
+	        Circuito c = new Circuito();
+	        /*c.rangoTiempo[0] = c.rangoTiempo[0].ofSeconds(3600);
+	        System.out.println(c.rangoTiempo[0].toHours());
+	        System.out.println(c.rangoTiempo[0].toMinutes());*/
 
-    }
+	    }
 
-}
+	}
--- src/datos/Coche.java
+++ src/datos/Coche.java
@@ -4,101 +4,101 @@ package datos;
 
 public class Coche {
 
-    protected String nombre;
-    protected String abreviado;
-    protected String escuderia;
-    protected double velocidad;
-    protected double aceleracion;
-    protected double aerodinamica;
-    protected double probRotura;
-    protected double neumaticos;
+	  protected String nom_piloto;
+	    protected String abreviado;
+	    protected String escuderia;
+	    protected double velocidad;
+	    protected double aceleracion;
+	    protected double aerodinamica;
+	    protected double prob_rotura;
+	    protected double neumaticos;
 
-    /**
-     * Contructor vacio con valores predeterminados
-     */
-    public Coche(){
-        velocidad = 0;
-        aceleracion = 0;
-        aerodinamica = 0;
-        probRotura = 0;
-    }
+	    /**
+	     * Contructor vacio con valores predeterminados
+	     */
+	    public Coche(){
+	        velocidad = 0;
+	        aceleracion = 0;
+	        aerodinamica = 0;
+	        prob_rotura = 0;
+	    }
 
-    /**
-     * Contructor para la creacion personalizada de un piloto
-     * @param nombre
-     * @param abreviado
-     * @param escuderia
-     * @param velocidad
-     * @param aceleracion
-     * @param aerodinamica
-     * @param probRotura
-     */
-    public Coche(String nombre, String abreviado, String escuderia, double velocidad, double aceleracion, double aerodinamica, double probRotura, double neumaticos) {
-        this.nombre = nombre;
-        this.abreviado = abreviado;
-        this.escuderia = escuderia;
-        this.velocidad = velocidad;
-        this.aceleracion = aceleracion;
-        this.aerodinamica = aerodinamica;
-        this.probRotura = probRotura;
-        this.neumaticos = neumaticos;
-    }
+	    /**
+	     * Contructor para la creacion personalizada de un piloto
+	     * @param nombre
+	     * @param abreviado
+	     * @param escuderia
+	     * @param velocidad
+	     * @param aceleracion
+	     * @param aerodinamica
+	     * @param probRotura
+	     */
+	    public Coche(String nombre, String abreviado, String escuderia, double velocidad, double aceleracion, double aerodinamica, double probRotura, double neumaticos) {
+	        this.nom_piloto = nombre;
+	        this.abreviado = abreviado;
+	        this.escuderia = escuderia;
+	        this.velocidad = velocidad;
+	        this.aceleracion = aceleracion;
+	        this.aerodinamica = aerodinamica;
+	        this.prob_rotura = probRotura;
+	        this.neumaticos = neumaticos;
+	    }
 
-    public String getNombre() {
-        return nombre;
-    }
+	    public String getNombre() {
+	        return nom_piloto;
+	    }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	    public void setNombre(String nombre) {
+	        this.nom_piloto = nombre;
+	    }
 
-    public String getAbreviado() { return abreviado; }
+	    public String getAbreviado() { return abreviado; }
 
-    public void setAbreviado(String abreviado) { this.abreviado = abreviado; }
+	    public void setAbreviado(String abreviado) { this.abreviado = abreviado; }
 
-    public String getEscuderia() {
-        return escuderia;
-    }
+	    public String getEscuderia() {
+	        return escuderia;
+	    }
 
-    public void setEscuderia(String escuderia) {
-        this.escuderia = escuderia;
-    }
+	    public void setEscuderia(String escuderia) {
+	        this.escuderia = escuderia;
+	    }
 
-    public double getVelocidad() {
-        return velocidad;
-    }
+	    public double getVelocidad() {
+	        return velocidad;
+	    }
 
-    public void setVelocidad(double velocidad) {
-        this.velocidad = velocidad;
-    }
+	    public void setVelocidad(double velocidad) {
+	        this.velocidad = velocidad;
+	    }
 
-    public double getAceleracion() {
-        return aceleracion;
-    }
+	    public double getAceleracion() {
+	        return aceleracion;
+	    }
 
-    public void setAceleracion(double aceleracion) {
-        this.aceleracion = aceleracion;
-    }
+	    public void setAceleracion(double aceleracion) {
+	        this.aceleracion = aceleracion;
+	    }
 
-    public double getAerodinamica() {
-        return aerodinamica;
-    }
+	    public double getAerodinamica() {
+	        return aerodinamica;
+	    }
 
-    public void setAerodinamica(double aerodinamica) {
-        this.aerodinamica = aerodinamica;
-    }
+	    public void setAerodinamica(double aerodinamica) {
+	        this.aerodinamica = aerodinamica;
+	    }
 
-    public double getProbRotura() {
-        return probRotura;
-    }
+	    public double getProbRotura() {
+	        return prob_rotura;
+	    }
 
-    public void setProbRotura(double probRotura) {
-        this.probRotura = probRotura;
-    }
+	    public void setProbRotura(double probRotura) {
+	        this.prob_rotura = probRotura;
+	    }
 
-    public double getNeumaticos() { return neumaticos;
-    }
+	    public double getNeumaticos() { return neumaticos;
+	    }
 
-    public void setNeumaticos(double neumaticos) { this.neumaticos = neumaticos;
-    }
-}
+	    public void setNeumaticos(double neumaticos) { this.neumaticos = neumaticos;
+	    }
+	}
